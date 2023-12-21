e9cf8f17b14a358e455bb1f8936ae00490cdd54c
tiphaine pinson <tiphaine.pinson@hotmail.fr>
2023-12-21 09:58:32 +0100

    ajout photo footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.scss';
+import './styles/index.css';
 import App from "./App";
 
 
@@ -10,5 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
 
 
