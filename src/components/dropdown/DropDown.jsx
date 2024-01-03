609232839ff199fbb6fa72c452aae263d50cad32
tiphaine pinson <tiphaine.pinson@hotmail.fr>
2024-01-03 18:34:31 +0100

    ajout du style pour le dropDown

---
+++ src/components/dropdown/DropDown.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import arrowUp from "../../assets/images/arrow_up.png";
+import arrowDown from "../../assets/images/arrow_down.png";
+
+const DropDown = ({ title, content, isList = false, open= false }) => {
+  // Using state to keep track of dropdown status (open or closed)
+  const [isOpen, setIsOpen] = useState(open);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <article className="dropDown">
+      <div className="dropDown__header" onClick={toggleOpen}>
+        <h3 className="dropDown__title">{title}</h3>
+        {isOpen ? (
+          <img src={arrowUp} alt="fermé" />
+        ) : (
+          <img src={arrowDown} alt="ouvert" />
+        )}
+      </div>
+      {/* Displaying content of the dropdown if it is open */}
+      {isList === true
+        ? isOpen && <ul className="dropDown__content">{content}</ul>
+        : isOpen && <p className="dropDown__content">{content}</p>}
+    </article>
+  );
+};
+
+export default DropDown;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import BannerImg from "../components/banner/BannerImg";
+import DropDown from "../components/dropdown/DropDown";
+import { dropDownData } from "../data/dropDownData";
 
-
+const DropDownAbout = ({ data }) => {
+  return (
+    <section>
+      {data.map((item, index) => (
+        <DropDown key={index} title={item.title} content={item.paragraph} />
+      ))}
+    </section>
+  );
+};
 
 const About = () => {
   return (
     <main>
       <BannerImg />
+      <DropDownAbout data={dropDownData} />
     </main>
   );
 };
-
 
 export default About;
